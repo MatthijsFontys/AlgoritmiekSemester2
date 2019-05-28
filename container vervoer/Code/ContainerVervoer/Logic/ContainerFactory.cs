eae61cbdb26026394a0bd8d1d7e73b2c0b45c1e5
Matthijs <matthijs.3mail@gmail.com>
2019-05-28 12:15:08 +0200

    Added Container factory

---
+++ container vervoer/Code/ContainerVervoer/Logic/ContainerFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic {
+    class ContainerFactory {
+        public static IEnumerable<IContainer> CreateValuableContainers(double weight, int amount = 1) {
+            List<IContainer> toReturn = new List<IContainer>();
+            for (int i = 0; i < amount; i++)
+                toReturn.Add(new ValuableContainer(weight));
+            return toReturn;
+        }
+
+        public static IEnumerable<IContainer> CreateRegularContainers(double weight, int amount = 1) {
+            List<IContainer> toReturn = new List<IContainer>();
+            for (int i = 0; i < amount; i++)
+                toReturn.Add(new RegularContainer(weight));
+            return toReturn;
+        }
+
+        public static IEnumerable<IContainer> CreateCooledContainers(double weight, int manditoryY , int amount = 1){
+            List<IContainer> toReturn = new List<IContainer>();
+            for (int i = 0; i < amount; i++)
+                toReturn.Add(new CooledContainer(manditoryY, weight));
+            return toReturn;
+        }
+
+        public static IEnumerable<IContainer> CreateCooledContainers(double weight, Ship ship, int amount = 1) {
+            return CreateCooledContainers(weight, ship.Length, amount);
+        }
+
+        public static IEnumerable<IContainer> ConcatContainerTypes(params IEnumerable<IContainer>[] lists) {
+            int finalCapacity = 0;
+            foreach (IEnumerable<IContainer> list in lists)
+                finalCapacity += list.Count();
+
+            List<IContainer> toReturn = new List<IContainer>(finalCapacity);
+
+            foreach (IEnumerable<IContainer> list in lists)
+                toReturn.AddRange(list);
+
+            return toReturn;
+        }
+
+
+    }
+}
