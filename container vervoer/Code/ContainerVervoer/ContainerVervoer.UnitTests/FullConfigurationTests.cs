238b097d150eddc78ff0eb151feed994a9059575
Matthijs <matthijs.3mail@gmail.com>
2019-05-31 11:58:26 +0200

    Made a few full configuration unit tests

--- container vervoer/Code/ContainerVervoer/ContainerVervoer.UnitTests/FullConfigurationTests.cs
+++ container vervoer/Code/ContainerVervoer/ContainerVervoer.UnitTests/FullConfigurationTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,57 @@ namespace ContainerVervoer.UnitTests {
 
     [TestClass]
     public class FullConfigurationTests {
+        LoadStrategy loadStrategy;
+        List<IContainer> containers;
+
+        [TestInitialize]
+        public void Setup() {
+            loadStrategy = new LoadStrategy();
+            containers = new List<IContainer>();
+        }
+
+
+        [TestMethod]
+        // https://i872272.venus.fhict.nl/ContainerVisualizer/index.html?length=4&width=5&stacks=2,2,2,2/12,12,12,12/112,112,112,112/12,12,12,12/2,2,2,2
+        public void FullShipWithReachableCooled() {
+            Ship ship = new Ship(5, 4);
+            containers = ContainerFactory.ConcatContainerTypes(
+                ContainerFactory.CreateValuableContainers(30, 20),
+                ContainerFactory.CreateRegularContainers(30, 16)
+                );
+            loadStrategy.DivideContainers(containers, ship);
+
+            Assert.IsTrue(ship.Validate());
+        }
+
+        [TestMethod]
+        // https://i872272.venus.fhict.nl/ContainerVisualizer/index.html?length=2&width=2&stacks=1111111,111111/1111111,111111
+        public void FillUpWeightEntireBoat() {
+            Ship ship = new Ship(2, 2);
+            containers = ContainerFactory.ConcatContainerTypes(
+                ContainerFactory.CreateRegularContainers(30, 16),
+                ContainerFactory.CreateRegularContainers(20, 4),
+                ContainerFactory.CreateRegularContainers(8, 2),
+                ContainerFactory.CreateRegularContainers(6, 2),
+                ContainerFactory.CreateRegularContainers(4, 2)
+                );
+            loadStrategy.DivideContainers(containers, ship);
+
+            Assert.IsTrue(ship.Validate());
+        }
+
+        [TestMethod]
+        // https://i872272.venus.fhict.nl/ContainerVisualizer/index.html?length=4&width=5&stacks=2,2,2,2/,,,12/,,,33333333/,,,/2,2,2,2
+        public void AllContainerTypes() {
+            Ship ship = new Ship(5, 4);
+            containers = ContainerFactory.ConcatContainerTypes(
+             ContainerFactory.CreateValuableContainers(4, 9),
+             ContainerFactory.CreateCooledContainers(4, 4, 8),
+             ContainerFactory.CreateRegularContainers(4, 1)
+             );
+            loadStrategy.DivideContainers(containers, ship);
+            Assert.IsTrue(ship.Validate());
+        }
 
     }
 }
